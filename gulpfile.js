15c9f0572ff30344ba0dc58c365c0979abad27e1
Jhonier Rojas <yonier202@gmail.com>
2024-08-28 21:45:11 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 import gulpSass from 'gulp-sass';
 import * as dartSass from "sass";
-import {src, dest, watch} from 'gulp';
+import {src, dest, watch, series} from 'gulp';
 
 const sass = gulpSass(dartSass);
 
 export function css(done) {
-    src('src/scss/app.scss')
+    src('src/scss/app.scss', {sourcemaps: true})
         .pipe(sass().on('error',sass.logError))
-        .pipe(dest('build/css'))
+        .pipe(dest('build/css', {sourcemaps: true}))
+    done();
+}
+export function js(done){
+    src('src/js/app.js')
+        .pipe(dest('build/js'))
     done();
 }
 
 export function dev() {
     //compilar todos los archivos que esten en la carpeta scss con extención scss
     watch('src/scss/**/*.scss',css);
+    watch('src/js/**/*.js',js);
 }
+
+export default series(js, css, dev);
 
